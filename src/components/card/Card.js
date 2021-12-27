1f077c8f956e39753c1d31a7f230284c20153b04
garoqui <quiroga_alejandro@hotmail.com>
2021-12-27 12:16:48 +0100

    fixed branch!

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,12 +1,8 @@
 import { LitElement, html, css } from 'lit';
 import { Router } from '@vaadin/router';
 
-
-
 import { MOVIESIMGURLW500 } from '../../constants/api.js';
-import 'fa-icons';
 
-// components
 import '../ranking/app-ranking.js';
 
 export class Card extends LitElement {
@@ -16,7 +12,6 @@ export class Card extends LitElement {
     this.imgMovie = '';
     this.kindOfList = '';
     this.noimage = '/assets/no-image-found.png';
-    this.favorites = [];
   }
 
   static get properties() {
@@ -25,7 +20,6 @@ export class Card extends LitElement {
       items: { type: Object },
       kindOfList: { type: String },
       noimage: { type: String },
-      favorites: { type: Array },
     };
   }
 
@@ -47,47 +41,23 @@ export class Card extends LitElement {
       .container-ranking {
         display: flex;
         width: 100%;
-        justify-content: space-evenly;
-      }
-      .rk-1 {
-        width: 15%;
-      }
-      .rk-2 {
-        width: 40%;
-      }
-      .rk-3 {
-        width: 15%;
-      }
-      fa-icon {
-        color: #ff0055;
-        filter: grayscale(100%);
+        /* justify-content: space-evenly; */
       }
 
-      .isfavorite {
-        opacity: 1;
-        filter: grayscale(5%);
-      }
+      /* @media only screen and (max-width: 988px) {
+        .container {
+          flex-wrap: wrap;
+        }
+      } */
     `;
   }
 
   goDetail() {
-    console.log(this.item);
     Router.go({
       pathname: '/detail',
       search: `?id=${this.item.id}&kind=${this.item.media_type}`,
     });
   }
-
-<<<<<<< Updated upstream
-  setFavorite() {
-    store.dispatch(addFavorite(this.item));
-    this.isFavorite(this.item);
-  }
-=======
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
 
   stateChanged(state) {
     this.favorites = state.reducer.favorites;
@@ -101,72 +71,39 @@ export class Card extends LitElement {
     return false;
   }
 
-<<<<<<< Updated upstream
-=======
->>>>>>> Stashed changes
->>>>>>> Stashed changes
   render() {
     // eslint-disable-next-line lit-a11y/click-events-have-key-events
-    return html`<div class="container-card">
-      <div @click=${this.goDetail}>
-        <img
-          alt=${this.item.title}
-          src=${this.item.backdrop_path
-            ? MOVIESIMGURLW500 + this.item.backdrop_path
-            : this.noimage}
-        />
-        ${this.item.name
-          ? html`<div><strong>${this.item.name}</strong></div>`
-          : html`<div><strong>${this.item.title}</strong></div>`}
-      </div>
+    return html`<div class="container-card" @click=${this.goDetail}>
+      <img
+        alt=${this.item.title}
+        src=${this.item.backdrop_path
+          ? MOVIESIMGURLW500 + this.item.backdrop_path
+          : this.noimage}
+      />
+      ${this.item.name
+        ? html`<div><strong>${this.item.name}</strong></div>`
+        : html`<div><strong>${this.item.title}</strong></div>`}
       <div class="container-ranking">
-<<<<<<< Updated upstream
-=======
-<<<<<<< Updated upstream
-        <div>${this.item.vote_average}</div>
-        <div><app-ranking .rate=${this.item.vote_average}></app-ranking></div>
-=======
->>>>>>> Stashed changes
         <div class="rk-1">${this.item.vote_average}</div>
         <div class="rk-2">
           <app-ranking .rate=${this.item.vote_average}></app-ranking>
         </div>
         <div>
           ${this.isFavorite(this.item)
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-            ? html` <div class="rk-3" @click=${this.setFavorite}>
-=======
-            ? html` <div class="rk-3" >
->>>>>>> Stashed changes
-=======
-            ? html` <div class="rk-3" >
->>>>>>> 5842f1b41f92856df1c6d7ce4e40553a998c05fa
+            ? html` <div class="rk-3">
                 <fa-icon
                   @click=${this.addMarginSlider}
                   class="fa fa-heart  isfavorite"
                 ></fa-icon>
                 <div></div>
               </div>`
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-            : html`<div class="rk-3" @click=${this.setFavorite}>
-=======
             : html`<div class="rk-3">
->>>>>>> Stashed changes
-=======
-            : html`<div class="rk-3" >
->>>>>>> 5842f1b41f92856df1c6d7ce4e40553a998c05fa
                 <fa-icon
                   @click=${this.addMarginSlider}
                   class="fa fa-heart"
                 ></fa-icon>
               </div>`}
         </div>
-<<<<<<< Updated upstream
-=======
->>>>>>> Stashed changes
->>>>>>> Stashed changes
       </div>
     </div>`;
   }
